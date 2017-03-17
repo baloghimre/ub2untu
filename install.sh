64e054ba2490bc21d312b26ed9a2696a7a1a2178
Imre Balogh <baloghi@edutech.hu>
2017-03-17 12:49:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,13 +28,9 @@ echo ""
 echo "            A telepítés azonnal elindul..."
 
 
+echo ""
 
-sleep 2
-#megkérdezi hogy el akarjuk inditani e a telepitést igen/nem 
-#igen update.sh 
-#nem kilép
-
-read -r -p "${1:-Biztos vagy benne hogy elinditod a telepitést? [y/N]} " response
+read -r -p "   ${1:-Biztos vagy benne hogy elinditod a telepitést? [y/N]} " response
 case "$response" in
 [yY][eE][sS]|[yY]) 
     	echo "a telepito most elindul"
