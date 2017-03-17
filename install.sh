1ac3727ac06a4ce61346049106cf404d1dbee08b
revkusz <revkusz@gmail.com>
2017-03-17 11:45:59 +0100

    install.sh készen van.

--- install.sh
+++ install.sh
@@ -27,8 +27,26 @@ echo "                        Pink Monkey"
 echo ""
 echo "            A telepítés azonnal elindul..."
 
-echo "$(date) - Telepítés megkezdése." > install.log
 
-sleep 5
 
-./update.sh
+sleep 2
+#megkérdezi hogy el akarjuk inditani e a telepitést igen/nem 
+#igen update.sh 
+#nem kilép
+
+read -r -p "${1:-Biztos vagy benne hogy elinditod a telepitést? [y/N]} " response
+case "$response" in
+[yY][eE][sS]|[yY]) 
+    	echo "a telepito most elindul"
+	echo "$(date) - Telepítés megkezdése." > install.log
+	./update.sh
+    ;;
+*)
+    	echo "$(date) - Telepítés megszakitása:install.sh-ban." > install.log
+	echo "a telepitő most kilep"
+    ;;
+esac
+
+
+
+
