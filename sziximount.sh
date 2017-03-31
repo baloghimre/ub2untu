6f4387d4ad4f92b5a54d3b9b0b269ddd848d0c57
Imre Balogh <baloghi@edutech.hu>
2017-03-31 11:41:51 +0200

    Add files via upload

---
+++ sziximount.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+input=$(zenity --text "Kérem add meg a felhasználó neved " \
+        --username --password)
+
+IFS='|' read -a myarray <<< "$input"
+felh=${myarray[0]}
+jelsz=${myarray[1]}
+
+
+
+#if [ "-u" = "$1" ]; then
+#read -p "Felhasználó név: " felh 
+
+ umount /media/$felh 
+ umount /media/kozos 
+ umount /media/mindenki 
+ 
+ rmdir /media/$felh 
+ rmdir /media/kozos 
+ rmdir /media/mindenki 
+	
+#else
+
+#read -p "Felhasználó név: " felh 
+#read -p "Jelszó:" -s jelsz
+
+
+mkdir -m 777 /media/tanulo/$felh 
+mkdir -m 777 /media/tanulo/kozos 
+mkdir -m 777 /media/tanulo/mindenki 
+
+
+
+sudo mount -t cifs //DELL-AD/iskola/tanulok/$felh /media/tanulo/$felh -o username=$felh,rw,password=$jelsz,domain=szechenyi.szixi.hu  #sajátmeghajtó
+
+sudo mount -t cifs //DELL-AD/iskola/kozos /media/tanulo/kozos -o username=$felh,rw,password=$jelsz,domain=szechenyi.szixi.hu  #kozos meghajtó
+
+sudo mount -t cifs //DELL-AD/mindenki /media/tanulo/mindenki -o username=$felh,rw,password=$jelsz,domain=szechenyi.szixi.hu #mindenki meghajtó
+
+echo $felh > /tmp/felh.txt
+
+#fi
+
+
+
